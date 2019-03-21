ad432fd4ae1ff44db0182e089d98336d8e37bdbe
domcabitac <dominic.cabitac@uoit.net>
2019-03-21 17:05:01 -0400

    Added toggle to all of the pictures
    Tried to do append on JS but wasnt working so i commented it out

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,39 @@
-$(document).ready(function(){
- $(".CAT").click(function(){
-  alert("it works!");
- });
-});
+// $(document).ready(function myFunction() {
+  // // $(".")
+  // $(".BOOKME").append("<p>SELECT A STYLE</p>");
+  // $(".BOOKME").append("<table align='center' class='tableBook' id='TABLE'>");
+  // $("#TABLE").append("<tr><td>" + "<img id='CAT' src='../images/Cat.jpg'>" + "</td>" + "<td>" 
+  //                     + "<img class='DOLL' src='../images/Doll.jpg'>" + "</td>" + "</tr>" + "<tr>" +"<td>" 
+  //                     + "<img class='NATURAL' src='../images/Natural.jpg'>" + "</td>" + "<td>" 
+  //                     + "<img class='OPEN' src='../images/Open.jpg'>" + "</td>" +"</tr>");
+  // $(".BOOKME").append("</table>")
+  // $(".BOOKME").addClass("BOOKME2");
+  // $("#TABLE").addClass("tableBook")
+  // $(".DOLL").toggle();
+  // $(".DOLL").toggle();
+// });
+
+function myFunction(){
+  $(".DOLL").toggle();
+  $(".NATURAL").toggle();
+  $(".OPEN").toggle();
+}
+
+function myFunction2(){
+  $(".CAT").toggle();
+  $(".NATURAL").toggle();
+  $(".OPEN").toggle();
+}
+
+function myFunction3(){
+  $(".CAT").toggle();
+  $(".DOLL").toggle();
+  $(".OPEN").toggle();
+}
+
+function myFunction4(){
+  $(".CAT").toggle();
+  $(".NATURAL").toggle();
+  $(".DOLL").toggle();
+}
+
