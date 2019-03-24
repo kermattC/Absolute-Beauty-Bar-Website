f38646e3eb7eaf162239ee03194ad2e7073e3b13
domcabitac <dominic.cabitac@uoit.net>
2019-03-24 11:30:20 -0400

    Added border to eyelash pictures when selected
    Theres an issue, I cant figure out how to remove them if another one is clicked

--- scripts/test.js
+++ scripts/test.js
@@ -14,15 +14,18 @@ $('img').click(function(event){
     $('#newForm').remove();;
     console.log("removing form");
   }
+    var element = document.getElementById(event.target.id);
+    element.classList.toggle("selected");
 
     let newForm = $(document.createElement("div"));
     newForm.attr('id', 'newForm');
     newForm.addClass('boxBooking');
     newForm.append($("<p> You have selected style: " + event.target.id + "</p>"));
-    newForm.append($("<form><br/>" + "FIRST NAME: " +"<input type='text' name='FirstName'>"));
+
+    newForm.append($("<form>" + "FIRST NAME: " +"<input type='text' name='FirstName'>"));
     newForm.append($("<br/>" + "LAST NAME: " +"<input type='text' name='lasttName'>" + "<br/>"));
     newForm.append($("<br/>" + "DATE: " +"<input type='date' name='date'>"));
-    newForm.append($("<br/> "+ "<input type='submit' id='SUBMIT' value='Confirm Booking'>" + "</form>"));
+    newForm.append($("<br/>"+ "<input type='submit' id='SUBMIT' value='Confirm Booking'>" + "</form>"));
   
     newForm.insertAfter($('#booking'));
 });
